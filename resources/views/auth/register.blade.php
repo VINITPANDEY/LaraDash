f62ba0d68a98c98419b553ca6856c982cc8a3ea6
VINITPANDEY <dev.vinit@outlook.com>
2022-02-21 22:42:29 +0530

    [Logout updtae]: Add logout function

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,6 +41,10 @@
             
               <div class="w-full">
                 <form method="POST" action="{{ route('register') }}">
+
+
+
+                  
                   @if(Session::get('success'))
                   <div class="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800" role="alert">
                     <span class="font-medium">Success</span> {{ Session::get('success') }}.
